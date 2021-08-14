b5a538c86cf19ce92a01e23f17933e7c87594bfb
Luca Casonato <hello@lcas.dev>
2021-08-14 14:21:51 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # urlpattern
-Rust implementation of the `URLPattern` web API
+
+This crate implements the [`URLPattern` web API][urlpattern] in Rust. We aim to
+follow [the specification][spec] as closely as possible.
+
+[urlpattern]: https://github.com/WICG/urlpattern
+[spec]: https://wicg.github.io/urlpattern/
+
+## Contributing
+
+We appreciate your help!
+
+The code of conduct from the Deno repository applies here too:
+https://github.com/denoland/deno/blob/main/CODE_OF_CONDUCT.md.
---
+++ src/lib.rs
@@ -0,0 +1,57 @@
+// Copyright 2018-2021 the Deno authors. All rights reserved. MIT license.
+
+use derive_more::Display;
+
+/// An error that occured during parsing.
+#[derive(Debug, Display)]
+pub enum ParseError {}
+
+impl std::error::Error for ParseError {}
+
+/// The structured input used to create a URL pattern.
+pub struct UrlPatternInit {
+  protocol: String,
+  username: String,
+  password: String,
+  hostname: String,
+  port: String,
+  pathname: String,
+  search: String,
+  hash: String,
+}
+
+impl UrlPatternInit {
+  pub fn parse(
+    _pattern: &str,
+    _base_url: Option<&str>,
+  ) -> Result<UrlPatternInit, ParseError> {
+    Ok(UrlPatternInit {
+      protocol: "".to_string(),
+      username: "".to_string(),
+      password: "".to_string(),
+      hostname: "".to_string(),
+      port: "".to_string(),
+      pathname: "".to_string(),
+      search: "".to_string(),
+      hash: "".to_string(),
+    })
+  }
+}
+
+/// A UrlPattern that can be matched against.
+pub struct UrlPattern {}
+
+impl UrlPattern {
+  /// Parse a [UrlPatternInit] and optionally a base url into a [UrlPattern].
+  pub fn parse(
+    _pattern_init: UrlPatternInit,
+    _base_url: Option<&str>,
+  ) -> Result<UrlPattern, ParseError> {
+    Ok(UrlPattern {})
+  }
+
+  /// Test if a given input string (with optional base url), matches the pattern.
+  pub fn test(&self, _input: &str, _base_url: Option<&str>) -> bool {
+    false
+  }
+}
